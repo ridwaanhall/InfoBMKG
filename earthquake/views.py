34470c4c39487dad387d514b155b45157dbaa90b
ridwaanhall <erbyl14@gmail.com>
2024-10-11 21:06:49 +0700

    Add earthquake API integration to earthquake views

--- earthquake/views.py
+++ earthquake/views.py
@@ -1,3 +1,20 @@
+import requests
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+BASE_URL = settings.BASE_URL
+
+def dashboard(request):
+    return render(request, 'earthquake/dashboard.html')
+
+def latest(request):
+    latest = requests.get(f'{BASE_URL}/datagempa.json')
+    latest = latest.json()
+    coordinates = latest['info']['point']['coordinates']
+    longitude, latitude = coordinates.split(',')
+    context = {
+        'latest': latest,
+        'longitude': longitude,
+        'latitude': latitude,
+    }
+    return render(request, 'earthquake/latest.html', context)

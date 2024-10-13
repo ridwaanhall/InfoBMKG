60d117fe318ec87d052c96cff65f79ea17841ef9
ridwaanhall <erbyl14@gmail.com>
2024-10-14 00:32:16 +0700

    Fetch latest earthquake data and fault information
    - Retrieve the latest earthquake data from the API and parse the coordinates.
    - Fetch fault information from the API.
    - Pass the retrieved data to the dashboard template for rendering.

--- earthquake/views.py
+++ earthquake/views.py
@@ -5,7 +5,20 @@ from django.conf import settings
 OUR_URL = settings.OUR_URL
 
 def dashboard(request):
-    return render(request, 'earthquake/dashboard.html')
+    latest = requests.get(f'{OUR_URL}/latest/')
+    latest = latest.json()
+    coordinates = latest['info']['point']['coordinates']
+    longitude, latitude = coordinates.split(',')
+    
+    fault_indo_world = requests.get(f'{OUR_URL}/fault-indo-world/')
+    fault_indo_world = fault_indo_world.json()
+    context = {
+        'latest': latest,
+        'longitude': longitude,
+        'latitude': latitude,
+        'fault_indo_world': fault_indo_world,
+    }
+    return render(request, 'earthquake/dashboard.html', context)
 
 def latest(request):
     latest = requests.get(f'{OUR_URL}/latest/')

c7876fd8d9146e59f57e2bd41367aaf8f0ac1ddb
ridwaanhall <erbyl14@gmail.com>
2024-10-13 14:57:53 +0700

    Refactor API endpoint URL in settings.py

--- InfoBMKG/settings.py
+++ InfoBMKG/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 BASE_URL = config('BASE_URL')
+OUR_URL = config('OUR_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/

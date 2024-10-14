9cf4e9ef6ed8853c498a37e39de4a8f095ee7b03
ridwaanhall <erbyl14@gmail.com>
2024-10-14 19:37:24 +0700

    Refactor API endpoint URL in urls.py

--- earthquake/urls.py
+++ earthquake/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # earthquake/urls.py
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
     path('latest/', views.latest, name='latest-quake'),
     # other URL patterns
 ]

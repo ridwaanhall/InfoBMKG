47646d56ebfa25cc7869817433fac0b3df471a0d
ridwaanhall <erbyl14@gmail.com>
2024-12-07 21:25:25 +0700

    Refactor URL path names for consistency and clarity

--- geoscience_api/urls.py
+++ geoscience_api/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('mmi-map/', views.MMIMap.as_view(), name='mmi-map'),
 
     # histories
-    path('last3months/', views.LessThan3MonthsQuakes.as_view(), name='last-3-months-quake'),
-    path('last5years/', views.LessThan5YearsQuakes.as_view(), name='last-5-years-quake'),
+    path('last3months/', views.LessThan3MonthsQuakes.as_view(), name='last3months'),
+    path('last5years/', views.LessThan5YearsQuakes.as_view(), name='last5years'),
     
     # sensors
     path('seismic-sensor-bmkg/', views.SeismicSensorBMKG.as_view(), name='seismic-sensor-bmkg'),
@@ -33,10 +33,10 @@ urlpatterns = [
     path('destructive-epicenter/', views.DestructiveEarthquakesEpicenter.as_view(), name='destructive-earthquakes-epicenter'),
     
     # events
-    path('live200/', views.Live200Events.as_view(), name='live-200-events'),
-    path('last30felt/', views.Last30FeltEvents.as_view(), name='last-30-felt'),
-    path('last30/', views.Last30Events.as_view(), name='last-30-events'),
-    path('last30stunami/', views.Last30TsunamiEvents.as_view(), name='last-30-tsunami'),
+    path('live200/', views.Live200Events.as_view(), name='live200'),
+    path('last30felt/', views.Last30FeltEvents.as_view(), name='last30felt'),
+    path('last30/', views.Last30Events.as_view(), name='last30'),
+    path('last30tsunami/', views.Last30TsunamiEvents.as_view(), name='last30tsunami'),
     
     # geojson
     path('indo-fault-lines/', views.IndoFaultsLines.as_view(), name='indo-fault-lines'),

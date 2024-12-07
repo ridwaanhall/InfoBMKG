dc559662855a98b8d34b34243fb49455d6226ebe
ridwaanhall <erbyl14@gmail.com>
2024-12-07 21:19:22 +0700

    Add redirect from root URL to /quake/ for improved navigation

--- InfoBMKG/urls.py
+++ InfoBMKG/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_quake(request):
+    return redirect('/quake/')
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/', include('geoscience_api.urls')),
     path('quake/', include('earthquake.urls')),
     # path('weather/', include('weather.urls')),
+    path('', redirect_to_quake),
 ]

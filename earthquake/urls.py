439d34de48d1ceb9499366823ecaa67d99e915b9
ridwaanhall <erbyl14@gmail.com>
2024-12-07 15:17:47 +0700

    Add last30felt URL path to urlpatterns for fetching last 30 felt events

--- earthquake/urls.py
+++ earthquake/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.dashboard_html, name='dashboard-quake'),
     path('latest/', views.latest_html, name='latest-quake'),
     path('live200/', views.live200_html, name='live200-quake'),
+    path('last30felt/', views.last30felt_html, name='last30felt-quake'),
 ]

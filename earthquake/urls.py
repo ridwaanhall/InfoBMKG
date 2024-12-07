78d79b02213be866d8b9a93543093cb7877cb882
ridwaanhall <erbyl14@gmail.com>
2024-12-07 22:20:44 +0700

    Add URL paths for last 30 events and last 30 tsunamis

--- earthquake/urls.py
+++ earthquake/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('latest/', views.latest_html, name='latest-quake'),
     path('live200/', views.live200_html, name='live200-quake'),
     path('last30felt/', views.last30felt_html, name='last30felt-quake'),
+    path('last30/', views.last30_html, name='last30-quake'),
+    path('last30tsunami/', views.last30tsunami_html, name='last30tsunami-quake'),
 ]

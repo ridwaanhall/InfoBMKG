31b3870a6092c25fdf35a61dd880199858775b7a
ridwaanhall <erbyl14@gmail.com>
2024-12-08 20:17:11 +0700

    Add user-related fields and timestamps to Subscriber model

--- earthquake/models.py
+++ earthquake/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 class Subscriber(models.Model):
     chat_id = models.CharField(max_length=100, unique=True)
+    user_id = models.CharField(max_length=100, blank=True, null=True)
+    username = models.CharField(max_length=100, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    
+    date_created = models.DateTimeField(default=timezone.now)
+    last_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.chat_id
+        return f'{self.chat_id} ({self.date_created})'

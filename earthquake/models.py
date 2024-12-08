f9b5c16a578db2b5a2c9eb9a4a678cd8ff3ce071
ridwaanhall <erbyl14@gmail.com>
2024-12-08 17:09:15 +0700

    Add Subscriber model to handle unique chat IDs

--- earthquake/models.py
+++ earthquake/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Subscriber(models.Model):
+    chat_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.chat_id
